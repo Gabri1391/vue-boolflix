950faabdfd375a70ee8b2c7cbcc0d3482aed4059
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-28 17:06:50 +0200

    Import Axios + Milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
+ <div>
   <div class="container my-5 d-flex justify-content-between align-center">
      <div class="logo">
       BOOLFLIX
      </div>
      <div>
-      <input type="text">
-     <button class="btn">CERCA</button>
+      <input type="text" v-model="searchQuery" placeholder="Insert a keyword...">
+     <button @click="callApi" class="btn">SEARCH</button>
      </div>
   </div>
+     <div class="mt-5">
+     <ul>
+        <li class="p-2" v-for="movie in searchedContents" :key="movie.id">
+          <strong>Title: {{ movie.title }}</strong>
+          <ul>
+            <li><strong>Original title:</strong> {{ movie.original_title }}</li>
+            <li><strong>Language:</strong>{{ movie.original_language }}</li>
+            <li><strong>Vote:</strong> {{ movie.vote_average }}</li>
+          </ul>
+        </li>
+     </ul>
+     </div>
+ </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      searchQuery: "",
+      baseUri:"https://api.themoviedb.org/3",
+      apiKey: "ef9005034e403e3d4179e8aa9211d0a3",
+      searchedContents: []
+    }
+  },
+  methods:{
+    callApi(){
+      if (!this.searchQuery) return;
+
+      axios.get(`${this.baseUri}/search/movie/?api_key=${this.apiKey}&query=${this.searchQuery}`)
+      .then((res) => {
+        this.searchedContents = res.data.results;
+        
+      })
+      
+      this.searchQuery = "";
+    }
+  }
 }
 </script>
 
